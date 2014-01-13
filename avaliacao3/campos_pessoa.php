a5bd78211183480e9999284e9c7edbbe32d01cb0
priscianne <prisciannelima@gmail.com>
2014-01-13 18:47:27 -0200

    Create campos_pessoa.php

---
+++ avaliacao3/campos_pessoa.php
@@ -0,0 +1,33 @@
+Nome do produto
+<br/>
+<input type="text" name="nome" />
+<br/>
+<br/>
+
+Cor do produto:
+<br/>
+<input type="radio" name="sexo" value="rosa" checked="checked" /> 
+<label>rosa</label>
+<input type="radio" name="sexo" value="azul"/>
+azul
+<input type="radio" name="sexo" value="amarelo"/>
+amarelo
+<br/>
+<br/>
+<input type="checkbox" name="aceito" value="Sim" />
+Produto novo 
+<br/>
+<br/>
+opções de entrega: <br/>
+<select name="estado">
+    <option>5 Dias</option>
+    <option>10 Dias</option>
+    <option>15 Dias</option>
+</select>
+<br/>
+<br/>
+
+Detalhe do produto
+<br/>
+<textarea name="observacoes" MAXLENGTH="180">Escreva aque o detalhe do produto</textarea>
+<br/>
